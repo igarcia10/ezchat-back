ed37f787d4daf1ea5c5001197e579ff43347a12b
igarcia10 <ivangarciatecnico@gmail.com>
2019-09-15 21:53:50 +0200

    implemented uuid

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const io = require('socket.io')();
+const uuidv4 = require('uuid/v4');
 
 const PORT = process.env.PORT || 8080;
 
 io.on('connection', client => {
   client.on('username', username => {
     client.username = username;
-    io.emit('is_online', '🔵 <i>' + client.username + ' join the chat..</i>');
+    io.emit('system_message', { id: uuidv4(), text: `🔵 <i>${username} joined the chat..</i>` });
   });
 
   client.on('disconnect', username => {
     client.username = username;
-    io.emit('is_online', '🔴 <i>' + client.username + ' left the chat..</i>');
+    io.emit('system_message', { id: uuidv4(), text: `🔴 <i>${username} left the chat..</i>` });
   })
 
   client.on('chat_message', message => {
-    io.emit('chat_message', '<strong>' + client.username + '</strong>: ' + message);
+    io.emit('chat_message', { id: uuidv4(), text: `<strong>${client.username}</strong>: ${message}` });
   });
 
 });
